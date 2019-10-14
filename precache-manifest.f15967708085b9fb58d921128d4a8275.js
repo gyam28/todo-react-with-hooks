cb04549a7da5d98d4c7b437801d2233be4b11a86
Gina <fogasgeorgina@gmail.com>
2019-10-15 02:55:23 +0300

    Updates

---
+++ precache-manifest.f15967708085b9fb58d921128d4a8275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ed778b0f4939126aaf83b0ffed84b7",
+    "url": "/todo-react-with-hooks/index.html"
+  },
+  {
+    "revision": "87bf5e8d796a6858514e",
+    "url": "/todo-react-with-hooks/static/css/2.3e6eb34c.chunk.css"
+  },
+  {
+    "revision": "1e534d4239af44238764",
+    "url": "/todo-react-with-hooks/static/css/main.e80f6fe1.chunk.css"
+  },
+  {
+    "revision": "87bf5e8d796a6858514e",
+    "url": "/todo-react-with-hooks/static/js/2.a804bfa5.chunk.js"
+  },
+  {
+    "revision": "1e534d4239af44238764",
+    "url": "/todo-react-with-hooks/static/js/main.6280b3b2.chunk.js"
+  },
+  {
+    "revision": "4ef5897135089d6abfa6",
+    "url": "/todo-react-with-hooks/static/js/runtime-main.bcb57540.js"
+  }
+]);
